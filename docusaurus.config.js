e8414974f6488f4468b8c5eab863ce2ed0059f16
Eric Lee <ranye0719@gmail.com>
2022-09-03 00:54:13 +0800

    chore: update script and config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,9 +16,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Eric Lee", // Usually your GitHub org/user name.
+  organizationName: "yitinglee", // Usually your GitHub org/user name.
   projectName: "yitinglee.github.io", // Usually your repo name.
-  deploymentBranch: "master",
+  deploymentBranch: "gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
